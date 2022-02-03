f4b30c02db50d879b65566a3c2b69c70556df9d3
samvidprabhu <87456845+samvidprabhu@users.noreply.github.com>
2022-02-02 17:36:54 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,128 @@
+var SpeechRecognition = window.webkitSpeechRecognition;
+  
+var recognition = new SpeechRecognition();
+
+function start()
+{
+    document.getElementById("textbox").innerHTML = ""; 
+    recognition.start();
+} 
+ 
+recognition.onresult = function(event) {
+
+ console.log(event); 
+
+var Content = event.results[0][0].transcript;
+
+    document.getElementById("textbox").innerHTML = Content;
+    console.log(Content);
+      if(Content =="take my selfie")
+      {
+        console.log("taking selfie --- ");
+        speak();
+      }
+}
+
+
+function speak(){
+    var synth = window.speechSynthesis;
+
+    speak_data = "Taking you Selfie in 5 seconds";
+
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+
+    synth.speak(utterThis);
+
+    Webcam.attach(camera);
+
+    setTimeout(function()
+    { 
+        take_snapshot(); 
+        save();
+    }, 5000);
+}
+
+ 
+camera = document.getElementById("camera");
+Webcam.set({
+    width:360,
+    height:250,
+    image_format : 'jpeg',
+    jpeg_quality:90
+});
+
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri) {
+        document.getElementById("result").innerHTML = '<img id="selfie_image" src="'+data_uri+'"/>';
+    });
+}
+
+
+function save()
+{
+  link = document.getElementById("link");
+  image = document.getElementById("selfie_image").src ;
+  link.href = image;
+  link.click();
+}
+
+const firebaseConfig = {
+    apiKey: "AIzaSyD6HJcuHvPZeL_chD62JwCfTtPFrrem9Ac",
+    authDomain: "p100-7e55e.firebaseapp.com",
+    databaseURL: "https://p100-7e55e-default-rtdb.firebaseio.com",
+    projectId: "p100-7e55e",
+    storageBucket: "p100-7e55e.appspot.com",
+    messagingSenderId: "650951404897",
+    appId: "1:650951404897:web:667aa640b748ad523c6920",
+    measurementId: "G-R8KT92GL6E"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+
+  firebase.initializeApp(firebaseConfig);
+  
+  user_name = localStorage.getItem("user_name");
+  
+  document.getElementById("user_name").innerHTML = "Welcome " + user_name + "!";
+  
+  function addRoom()
+  {
+    room_name = document.getElementById("room_name").value;
+  
+    firebase.database().ref("/").child(room_name).update({
+      purpose : "adding room name"
+    });
+  
+      localStorage.setItem("room_name", room_name);
+      
+      window.location = "kwitter_page.html";
+  }
+  
+  function getData() {  firebase.database().ref("/").on('value', function(snapshot) { document.getElementById("output").innerHTML = ""; snapshot.forEach(function(childSnapshot) { childKey  = childSnapshot.key;
+         Room_names = childKey;
+         console.log("Room Name - " + Room_names);
+        row = "<div class='room_name' id="+Room_names+" onclick='redirectToRoomName(this.id)' >#"+ Room_names +"</div><hr>";
+        document.getElementById("output").innerHTML += row;
+      });
+    });
+  
+  }
+  
+  getData();
+  
+  function redirectToRoomName(name)
+  {
+    console.log(name);
+    localStorage.setItem("room_name", name);
+      window.location = "kwitter_page.html";
+  }
+  
+  function logout() {
+  localStorage.removeItem("user_name");
+  localStorage.removeItem("room_name");
+      window.location = "index.html";
+  }
+  
